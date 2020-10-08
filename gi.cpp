eebf2bb4f86a031c3dc28ffa4a8407ad601b2dd4
mratimuthi <71791683+mratimuthi@users.noreply.github.com>
2020-10-08 17:34:37 +0200

    Add files via upload

---
+++ gi.cpp
@@ -0,0 +1,13 @@
+//
+//  gi.cpp
+//  
+//
+//  Created by Mark Mane on 8.10.20.
+//
+
+#include "gi.hpp"
+#include <iostream>
+
+using namespace std;
+
+        cout << "Welcome to the mafiat server! As you can see everyone is gay rank. If you want to go up a rank you need to pay 5 dollars. If you want to go straight to the top rank you can pay 20 dollars. This money will go to discord nitro and will make the server better." <<
