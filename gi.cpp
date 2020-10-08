b9c7436ee8ea96a4c158b29ea8b7b3ba94a97149
mratimuthi <71791683+mratimuthi@users.noreply.github.com>
2020-10-08 17:36:36 +0200

    Update gi.cpp

--- gi.cpp
+++ gi.cpp
@@ -9,5 +9,8 @@
 #include <iostream>
 
 using namespace std;
-
-        cout << "Welcome to the mafiat server! As you can see everyone is gay rank. If you want to go up a rank you need to pay 5 dollars. If you want to go straight to the top rank you can pay 20 dollars. This money will go to discord nitro and will make the server better." <<
+int main ()
+{
+        cout << "Welcome to the mafiat server! As you can see everyone is gay rank. If you want to go up a rank you need to pay 5 dollars. If you want to go straight to the top rank you can pay 20 dollars. This money will go to discord nitro and will make the server better." << end1;
+        return 0;
+}
